f58e6c0ed40e3b052b38b55601995ad847302fdb
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-26 00:03:21 +0100

    sidebar styling with icons and hover states

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,20 @@
 function MainContainer() {
   return (
     <div id="container">
-      <aside>sidebar</aside>
+      <aside>
+        <div>
+          <i className="fa-solid fa-user fa-lg" id="user-icon"></i>
+          Användare
+        </div>
+        <div>
+          <i className="fa-solid fa-circle-plus fa-lg" id="add-task-icon"></i>
+          Lägg till uppgift
+        </div>
+        <div>
+          <i className="fa-solid fa-inbox fa-lg" id="inbox-icon"></i>
+          Inkorg
+        </div>
+      </aside>
       <main>main</main>
     </div>
   );
