5181befa8076c03f483b7fe0b1577967abba58a0
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-26 20:03:59 +0100

    preliminary logotype

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
 function Sidebar() {
   return (
     <aside>
+      <div className="logo-div">
+        <span className="fa-stack fa-2x">
+          <i className="fa-solid fa-square fa-stack-2x"></i>
+          <i className="fa-solid fa-check-double fa-stack-1x fa-inverse"></i>
+        </span>
+        <span>Todoish</span>
+      </div>
       <div>
         <i className="fa-solid fa-user fa-lg" id="user-icon"></i>
         <span>Användare</span>
