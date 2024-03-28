93cc5d81ab9db44c93e5ebea7442ca7ea03cdb52
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-28 12:44:22 +0100

    placed tasks list in own component

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -1,3 +1,4 @@
+import TasksList from "./TasksList.jsx";
 import AddTaskDropdown from "./AddTaskDropdown.jsx";
 
 function Inbox() {
@@ -6,52 +7,7 @@ function Inbox() {
       <h1>Inkorg</h1>
       <div id="tasks-list">
         <div>
-          <ul>
-            <li>
-              <input
-                id="radio-1"
-                type="radio"
-                name="radio-group"
-                className="radio-check"
-              />{" "}
-              <label htmlFor="radio-1" className="radio-check-label">
-                Dropdown i main
-              </label>
-            </li>
-            <li>
-              <input
-                id="radio-2"
-                type="radio"
-                name="radio-group"
-                className="radio-check"
-              />{" "}
-              <label htmlFor="radio-2" className="radio-check-label">
-                Modal i sidebar
-              </label>
-            </li>
-            <li>
-              <input
-                id="radio-3"
-                type="radio"
-                name="radio-group"
-                className="radio-check"
-              />{" "}
-              <label htmlFor="radio-3" className="radio-check-label">
-                Mockdata
-              </label>
-            </li>
-            <li>
-              <input
-                id="radio-4"
-                type="radio"
-                name="radio-group"
-                className="radio-check"
-              />{" "}
-              <label htmlFor="radio-4" className="radio-check-label">
-                Databas
-              </label>
-            </li>
-          </ul>
+          <TasksList />
           <div className="add-task-from-main" id="hover-effect">
             <button id="circle">
               <i className="fas fa-solid fa-plus" id="plus-sign-icon"></i>
